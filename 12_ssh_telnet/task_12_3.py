8c9775beb08782a7a4e90fd724d7943b43ff0b33
gorynya <antongorynya1@gmail.com>
2017-05-17 17:40:42 +0300

    send_command

--- 12_ssh_telnet/task_12_3.py
+++ 12_ssh_telnet/task_12_3.py
@@ -1,51 +1,85 @@
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
-'''
-Задание 12.3
-
-Создать функцию send_commands (для подключения по SSH используется netmiko).
-
-Функция ожидает аргументы:
-* devices_list - список словарей с параметрами подключения к устройствам, которым надо передать команды один из аргументов с командами:
- * show - одна команда show (строка)
- * filename - имя файла, в котором находятся команды, которые надо выполнить (строка)
- * config - список с командами, которые надо выполнить в конфигурационном режиме
-
-В зависимости от того, какой аргумент был передан, функция вызывает разные функции внутри.
-
-Далее комбинация из аргумента и соответствующей функции:
-* show -- функция send_show_command из задания 12.1
-* config -- функция send_config_commands из задания 12.2, 12.2a или 12.2b
-* filename -- функция send_commands_from_file (ее также надо написать по аналогии с предыдущими)
 
 
-Функция send_commands возвращает словарь с результатами выполнения команды:
-* ключ - IP устройства
-* значение - результат выполнения команды
+import netmiko 
+import yaml
 
-Проверить работу функции на примере:
-* устройств из файла devices.yaml (для этого надо считать информацию из файла)
-* и различных комбинация аргумента с командами:
-    * списка команд commands
-    * команды command
-    * файла config.txt
-'''
-
-from netmiko import ConnectHandler
-
-commands = [ 'logging 10.255.255.1',
-             'logging buffered 20010',
-             'no logging console' ]
+device_list = yaml.load(open('devices3.yaml'))
+commands = [ 'interface  loopback 0',
+             'shutdown']
+			 
 command = "sh ip int br"
 
+
+
+
 def send_show_command(device_list, show_command):
-    pass
+	result_dict = {}
+	for device in device_list.keys():
+		ssh = netmiko.ConnectHandler(**device_list[device])
+		ssh.enable()
+		result = str(ssh.send_command(show_command))
+		result_dict.update({device_list[device]['ip']: result})
+		#print(result_dict[device_list[device]['ip']])
+		
+	return(result_dict)
+
+
 
 def send_config_commands(device_list, config_commands, output=True):
-    pass
+	result_dict = {}
+	for device in device_list.keys():
+		ssh = netmiko.ConnectHandler(**device_list[device])
+		ssh.enable()	
+		result = ssh.send_config_set(config_commands)
+		
+		if result.count('Incomplete command'):
+			print ('Error during executing "%s" on %s: Incomplete command' % (config_commands, device_list[device]['ip']))
+		elif result.count('Ambiguous command'):
+			print ('Error during executing "%s" on %s: Ambiguous command' % (config_commands, device_list[device]['ip']))
+		elif result.count('Invalid input'):
+			print ('Error during executing "%s" on %s: Invalid input' % (config_commands, device_list[device]['ip']))
+		else:
+			result_dict.update({device_list[device]['ip']: result})
+			if output:
+				print({device_list[device]['ip']: result})	
+	return(result_dict)
 
-def send_commands_from_file(device_list, filename):
-    pass
+def send_commands_from_file(device_list, filename, output=True):
+	result_dict = {}
+	for device in device_list.keys():
+		ssh = netmiko.ConnectHandler(**device_list[device])
+		ssh.enable()	
+		result = ssh.send_config_from_file(filename)
+		
+		if result.count('Incomplete command'):
+			print ('Error during executing "%s" on %s: Incomplete command in file' % (filename, device_list[device]['ip']))
+		elif result.count('Ambiguous command'):
+			print ('Error during executing "%s" on %s: Ambiguous command' % (filename, device_list[device]['ip']))
+		elif result.count('Invalid input'):
+			print ('Error during executing "%s" on %s: Invalid input' % (filename, device_list[device]['ip']))
+		else:
+			result_dict.update({device_list[device]['ip']: result})
+			if output:
+				print({device_list[device]['ip']: result})		
+	return(result_dict)
 
 def send_commands(device_list, config=[], show='', filename=''):
-    pass
+	if config:
+		return send_config_commands(device_list, config, output=False)		
+	if show:
+		return send_show_command(device_list, show)		
+	if filename:
+		return send_commands_from_file(device_list, filename, output=True)
+	
+		
 
+if __name__ == "__main__":
+#	print("Config")
+#	print(send_commands(device_list,config = commands ))
+#	print("Show")
+#	print(send_commands(device_list,show = command))
+	print("From File")
+	print(send_commands(device_list,filename = 'config.txt' ))
+	
