ebb7f647e6c3fdb8452bc923cb3371ad3e5b776d
gorynya <antongorynya1@gmail.com>
2017-05-18 11:49:13 +0300

    Enter login\pass

--- 12_ssh_telnet/task_12_3a.py
+++ 12_ssh_telnet/task_12_3a.py
@@ -1,14 +1,97 @@
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
-'''
-Задание 12.3a
 
 
-Изменить функцию send_commands таким образом, чтобы в списке словарей device_list
-не надо было указывать имя пользователя, пароль, и пароль на enable.
+import netmiko 
+import yaml
+import getpass
 
-Функция должна запрашивать имя пользователя, пароль и пароль на enable при старте.
-Пароль не должен отображаться при наборе.
+device_list = yaml.load(open('devices3.yaml'))
+commands = [ 'interface  loopback 0',
+             'shutdown']
+			 
+command = "sh ip int br"
 
-В файле devices2.yaml эти параметры уже удалены.
-'''
+print("Hello!")
+username = input('Username: ')
+password = getpass.getpass('Password: ')
+enable_password = getpass.getpass('Enable password: ')
 
+for device_type in device_list.keys():
+	for device in device_list[device_type]:
+		device.update({'username':username})
+		device.update({'password':password})
+		device.update({'secret':enable_password})
+
+
+def send_show_command(device_list, show_command):
+	result_dict = {}
+	for device_type in device_list.keys():
+		for device in device_list[device_type]:
+			ssh = netmiko.ConnectHandler(**device)
+			ssh.enable()
+			result = str(ssh.send_command(show_command))
+			result_dict.update({device['ip']: result})
+			#print(result_dict[device['ip']])
+		
+	return(result_dict)
+
+
+
+def send_config_commands(device_list, config_commands, output=True):
+	result_dict = {}
+	for device_type in device_list.keys():
+		for device in device_list[device_type]:
+			ssh = netmiko.ConnectHandler(**device)
+			ssh.enable()	
+			result = ssh.send_config_set(config_commands)
+			
+			if result.count('Incomplete command'):
+				print ('Error during executing "%s" on %s: Incomplete command' % (config_commands, device['ip']))
+			elif result.count('Ambiguous command'):
+				print ('Error during executing "%s" on %s: Ambiguous command' % (config_commands, device['ip']))
+			elif result.count('Invalid input'):
+				print ('Error during executing "%s" on %s: Invalid input' % (config_commands, device['ip']))
+			else:
+				result_dict.update({device['ip']: result})
+				if output:
+					print({device['ip']: result})	
+	return(result_dict)
+
+def send_commands_from_file(device_list, filename, output=True):
+	result_dict = {}
+	for device_type in device_list.keys():
+		for device in device_list[device_type]:
+			ssh = netmiko.ConnectHandler(**device)
+			ssh.enable()	
+			result = ssh.send_config_from_file(filename)
+			
+			if result.count('Incomplete command'):
+				print ('Error during executing "%s" on %s: Incomplete command in file' % (filename, device['ip']))
+			elif result.count('Ambiguous command'):
+				print ('Error during executing "%s" on %s: Ambiguous command' % (filename, device['ip']))
+			elif result.count('Invalid input'):
+				print ('Error during executing "%s" on %s: Invalid input' % (filename, device['ip']))
+			else:
+				result_dict.update({device['ip']: result})
+				if output:
+					print({device['ip']: result})		
+	return(result_dict)
+
+def send_commands(device_list, config=[], show='', filename=''):
+	if config:
+		return send_config_commands(device_list, config, output=False)		
+	if show:
+		return send_show_command(device_list, show)		
+	if filename:
+		return send_commands_from_file(device_list, filename, output=True)
+	
+		
+
+if __name__ == "__main__":
+#	print("Config")
+#	print(send_commands(device_list,config = commands ))
+#	print("Show")
+#	print(send_commands(device_list,show = command))
+#	print("From File")
+	print(send_commands(device_list,filename = 'config.txt' ))	
