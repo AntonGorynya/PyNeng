db9893b68bb1c5ef77022421d51e71cf66d84d43
gorynya <antongorynya1@gmail.com>
2017-05-19 13:33:06 +0300

    fix_bugs

--- 10_data_structures/task_10_3.py
+++ 10_data_structures/task_10_3.py
@@ -8,13 +8,21 @@ if __name__ == "__main__":
 
 def parse_cdp_neighbors(string):
 	d = {}
-	hostname = string[:string.find(">")].strip()
-	list=string[string.find("Port ID")+8:].split('\n')	
-	for i in range(len(list)):
-		list[i]=list[i].split()			
-		if list[i] != []:
-			list[i] = [list[i][0],list[i][1]+list[i][2],list[i][-2]+list[i][-1]]		
-			d.update( {(list[i][1]):{list[i][0]:list[i][2]}})	
+	hostname = string[:string.find(">" or "#")].strip()	
+	raw_list=string[string.find("Port ID")+8:].split('\n')	
+	
+	#исправление бага если слишком длинный hostname		
+	for i in range(len(raw_list)):
+		raw_list[i]=raw_list[i].split()				
+		if len(raw_list[i]) == 1:
+			raw_list[i].extend(raw_list[i+1].split())			
+			raw_list[i+1] = "\n"
+		
+	for i in range(len(raw_list)):		
+	#	print(raw_list[i]," ",len(raw_list[i]), "\n")		
+		if raw_list[i] != []:
+			raw_list[i] = [raw_list[i][0],raw_list[i][1]+raw_list[i][2],raw_list[i][-2]+raw_list[i][-1]]		
+			d.update( {(raw_list[i][1]):{raw_list[i][0]:raw_list[i][2]}})	
 	d ={hostname:d}
 			
 	return d
